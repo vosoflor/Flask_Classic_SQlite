83dc3da10b12772d8333e0eaa52455ea68ffe890
Verónica Osorio Florián <vero_oso11@hotmail.com>
2023-02-05 14:58:14 -0500

    Base e index con estilos customizados

--- APP_IngresosGastos/routes.py
+++ APP_IngresosGastos/routes.py
@@ -1,5 +1,12 @@
+from flask import render_template
 from APP_IngresosGastos import app
 
 @app.route("/")
 def index():
-    return "Esto funciona, como mola Flask!!!"
+    dataBase = [
+        {"id":1, "Date":"2022-01-01", "Description":"Sueldo", "Value":1500},
+        {"id":2, "Date":"2022-01-05", "Description":"Regalo reyes", "Value":-150},
+        {"id":3, "Date":"2022-01-06", "Description":"Almuerzo", "Value":-100},
+    ]
+    return render_template("index.html", pageTitle = "Todos", dataBase = dataBase)
+
