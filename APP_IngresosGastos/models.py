74c13a644355a38414ca18bb5bf5205b00d501e6
Verónica Osorio Florián <vero_oso11@hotmail.com>
2023-02-05 21:15:48 -0500

    Select_all en capa modelo con conexión a SQL, template nuevo registro (formulario y validación de datos), template, ruta y función delete

---
+++ APP_IngresosGastos/models.py
@@ -0,0 +1,37 @@
+import sqlite3
+from APP_IngresosGastos.connection import ConnectDatabase
+from config import DATABASE
+
+def select_all():
+    connection = ConnectDatabase("SELECT id, Date, Description, Value FROM movements order by date;", None)
+
+    rows = connection.res.fetchall() # Se capturan filas de datos
+    columns = connection.res.description # Se capturan nombres de columnas
+
+    allRecords = []
+
+    for row in rows:
+        record = {}
+        for position, column in enumerate(columns):
+            record[column[0]] = row[position]
+        allRecords.append(record)
+    
+    connection.con.close()
+
+    return allRecords
+
+def insert(requestFormValues):
+    connection = ConnectDatabase("INSERT INTO movements(Date, Description, Value) VALUES(?, ?, ?)", requestFormValues)
+    connection.con.commit()
+    connection.con.close()
+
+def select_by(id):
+    connection = ConnectDatabase(f"SELECT id, Date, Description, Value FROM movements WHERE id={id}", None)
+    record = connection.res.fetchall()
+    connection.con.close()
+    return  record[0] 
+
+def delete_by(id):
+    connection = ConnectDatabase(f"DELETE FROM movements WHERE id={id}", None)
+    connection.con.commit()
+    connection.con.close()
--- APP_IngresosGastos/routes.py
+++ APP_IngresosGastos/routes.py
@@ -1,12 +1,42 @@
-from flask import render_template
+from flask import redirect, render_template, request, url_for
 from APP_IngresosGastos import app
+from APP_IngresosGastos.models import delete_by, insert, select_all, select_by
+from datetime import datetime, date
+
+def validateForm(requestForm):
+    error = []
+    requestDate = datetime.strptime(requestForm["Date"], '%Y-%m-%d').date()
+    if requestForm["Date"] == "" or requestDate > date.today():
+        error.append("Fecha inválida: La fecha introducida es en el futuro o está vacía")
+    if requestForm["Description"] == "":
+        error.append("Concepto vacío: Introduce una descripción")
+    if requestForm["Value"] == "" or float(requestForm["Value"]) == 0.0:
+        error.append("Cantidad vacío o cero: Debes introducir un monto válido")
+    return error
 
 @app.route("/")
 def index():
-    dataBase = [
-        {"id":1, "Date":"2022-01-01", "Description":"Sueldo", "Value":1500},
-        {"id":2, "Date":"2022-01-05", "Description":"Regalo reyes", "Value":-150},
-        {"id":3, "Date":"2022-01-06", "Description":"Almuerzo", "Value":-100},
-    ]
+    dataBase = select_all()
     return render_template("index.html", pageTitle = "Todos", dataBase = dataBase)
+
+@app.route("/new", methods=["GET", "POST"])
+def create():
+    if request.method == "GET":
+        return render_template("new.html", pageTitle = "Nuevo registro", requestForm = {})
+    else:
+        error = validateForm(request.form)
+        if error:
+            return render_template("new.html", pageTitle = "Nuevo registro", msgError = error, requestForm = request.form)
+        else:
+            insert([request.form["Date"], request.form["Description"], request.form["Value"]])
+            return redirect(url_for("index")) # Otra forma de dirigirse a otra URL llamando la función
+
+@app.route("/delete/<int:id>", methods=["GET", "POST"])
+def delete(id):
+    if request.method == "GET":
+        record = select_by(id)
+        return render_template("delete.html", pageTitle = "Eliminar registro", data = record)
+    else:
+        delete_by(id)
+        return redirect("/")
 
